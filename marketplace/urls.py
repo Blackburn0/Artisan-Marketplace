a5803cf2365b8d5e6f01d153abb82c6036f83826
Blackburn0 <hamzatadebayo5@gmail.com>
2024-07-24 15:28:42 +0100

    Initial commit

--- marketplace/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from .views import HomeView, StoreView
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('store/', StoreView.as_view(), name='store')
-]
