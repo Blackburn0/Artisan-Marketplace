a2af711707b099cfeec6f6c0a71c166e8a16d113
Blackburn0 <hamzatadebayo5@gmail.com>
2024-08-01 17:24:35 +0100

    new updates

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,18 +3,35 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User, Customer, Vendor
 
 class CustomerSignUpForm(UserCreationForm):
+    GENDER = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+    first_name = forms.CharField(max_length=255)
+    last_name = forms.CharField(max_length=255)
     address = forms.CharField(max_length=255)
+    phone_number = forms.IntegerField()
+    gender = forms.CharField(choices=GENDER)
+    profile_image = forms.ImageField()
 
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2', 'address')
+        fields = ('first_name', 'last_name','username', 'password1', 'password2', 'address', 'phone_number', 'gender', 'profile_image')
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.is_customer = True
         if commit:
             user.save()
-            Customer.objects.create(user=user, address=self.cleaned_data.get('address'))
+            Customer.objects.create(
+                user = user, 
+                first_name = self.cleaned_data.get('first_name'), 
+                last_name = self.cleaned_data.get('last_name'),
+                address = self.cleaned_data.get('address'), 
+                phone_number = self.cleaned_data.get('phone_number'), 
+                gender = self.cleaned_data.get('gender'), 
+                profile_image = self.cleaned_data.get('profile_image'), 
+            )
         return user
 
 class VendorSignUpForm(UserCreationForm):
--- accounts/models.py
+++ accounts/models.py
@@ -6,10 +6,26 @@ class User(AbstractUser):
     is_vendor = models.BooleanField(default=False)
 
 class Customer(models.Model):
+    GENDER = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
+    phone_number = models.IntegerField()
+    gender = models.CharField(max_length=6, choices=GENDER)
+    profile_image = models.ImageField(upload_to='Customer_Profile')
+
+    def __str__(self):
+        return self.first_name
 
 class Vendor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=15)
+    phone_number1 = models.IntegerField()
+    phone_number2 = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.company_name
